776c34b5f8bdd19228a9d2b37cc516eeb4b27326
vishesh270304 <vvce22ise0006@vvce.ac.in>
2025-05-26 12:53:37 +0530

    sort.py

---
+++ sort.py
@@ -0,0 +1,8 @@
+
+my_list = [5, 2, 9, 1, 7]
+my_list.sort()
+print("Sorted list using sort():", my_list)
+
+my_list2 = [3, 8, 4, 6, 0]
+sorted_list = sorted(my_list2)
+print("Sorted list using sorted():", sorted_list)
